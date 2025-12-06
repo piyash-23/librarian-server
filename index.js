7b90e148c6687750d3bad21197fcf4146ab6d4f9
piyash-23 <arafat.ahmed2004@gmail.com>
2025-12-06 10:30:19 +0600

    server for librarian

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Librarian Server running");
+});
+
+app.listen(PORT, () => {
+  console.log("server running");
+});
